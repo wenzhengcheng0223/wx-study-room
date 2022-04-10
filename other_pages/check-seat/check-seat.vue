4abef89f5ea27a37cc1de33d468e24e4bff64d33
wenzhengcheng <wenzhengcheng0223@163.com>
2022-04-10 20:37:44 +0800

    create check-seat

---
+++ other_pages/check-seat/check-seat.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		cheak seat
+		{{timestamp}}
+	</view>
+</template>
+
+<script>
+	export default {
+		props:{
+			timestamp:{
+				type:Number
+			}
+		},
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
