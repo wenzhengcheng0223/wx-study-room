4835d355cc85fa657b68313fe63023cf56a4b424
wenzhengcheng <wenzhengcheng0223@163.com>
2022-04-20 17:19:52 +0800

    添加 微信小程序分享功能

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ import Vue from 'vue'
 import {mapState,mapMutations} from 'vuex'
 import uView from 'uview-ui' // 引入全局uView
 Vue.use(uView)
+
 uni.$u.config.unit = 'rpx'
 
+let mpShare = require('uview-ui/libs/mixin/mpShare.js');
+Vue.mixin(mpShare)
 
 Vue.config.productionTip = false
 Vue.prototype.$store = store
