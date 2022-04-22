d84a5f326ae785b4b64864e0ba22f199c9e3c355
wenzhengcheng <wenzhengcheng0223@163.com>
2022-04-22 18:17:30 +0800

    增加 充值点券跳转路径

--- components/my-index-card/my-index-card.vue
+++ components/my-index-card/my-index-card.vue
@@ -2,14 +2,14 @@
 	<view>
 		<view class="my-card">
 			<view class="recharge">
-				<view class="my-card-bg">
+				<view class="my-card-bg" @click="toRecharge">
 					<view style="position: absolute; top: 20rpx; left: 20rpx;">
 						<u--text :bold="true" size="36" color="#ffffff" :selectable="false" text="充值点券" />
-						<u--text :bold="false" size="10" color="#0c9d16" :selectable="false" text=" 123" />
+						<view style="height: 10rpx;"></view>
 						<u--text :bold="false" size="24" color="#ffffff" :selectable="false" text="充值到余额" />
 					</view>
-					<view  style="position: absolute; bottom: 0px; right: 0px;">
-						<u-icon  name="/static/index/pay.png" size="104" color="#0ec61b"></u-icon>
+					<view style="position: absolute; bottom: 0px; right: 0px;">
+						<u-icon name="/static/index/pay.png" size="104" color="#0ec61b"></u-icon>
 					</view>
 				</view>
 			</view>
@@ -39,24 +39,32 @@
 
 <script>
 	export default {
-		name:"my-index-card",
+		name: "my-index-card",
 		data() {
 			return {
-				
+
 			};
+		},
+		methods: {
+			toRecharge() {
+				uni.navigateTo({
+					url: "/other_pages/recharge/recharge"
+				})
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
-	.my-card{
+	.my-card {
 		position: relative;
 		height: 200rpx;
 		padding: auto 40rpx;
 		padding-top: 0rpx;
 		// background-color: #2C405A;
 	}
-	.recharge{
+
+	.recharge {
 		position: absolute;
 		// top: 20px;
 		left: 40rpx;
@@ -64,33 +72,37 @@
 		// width: 20px;
 		// background-color: #ff5500;
 	}
-	.my-card-bg{
+
+	.my-card-bg {
 		position: absolute;
 		height: 200rpx;
 		width: 330rpx;
 		border-radius: 10rpx;
 		background-color: #0c9d16;
 	}
-	.recharge-right{
+
+	.recharge-right {
 		position: absolute;
 		right: 40rpx;
 		height: 200rpx;
 		width: 330rpx;
 	}
-	.pay-card{
+
+	.pay-card {
 		position: absolute;
 		height: 96rpx;
 		width: 330rpx;
 		border-radius: 5px;
 		background-color: #13bec1;
-		
+
 	}
-	.mt-card{
+
+	.mt-card {
 		position: absolute;
 		bottom: 0px;
 		height: 96rpx;
 		width: 330rpx;
 		border-radius: 10rpx;
 		background-color: #ff7801;
-	}	
+	}
 </style>
