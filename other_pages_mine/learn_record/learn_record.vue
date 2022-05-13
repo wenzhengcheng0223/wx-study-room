74984803c10e12f95afa51406ac003a2ccd7abf8
wenzhengcheng <wenzhengcheng0223@163.com>
2022-05-13 19:12:37 +0800

    完善 页面接口对接（进度100%）

--- other_pages_mine/learn_record/learn_record.vue
+++ other_pages_mine/learn_record/learn_record.vue
@@ -3,7 +3,7 @@
 		<my-navbar leftText="我的记录" leftIcon="arrow-left" :custom="false" bgColor="#ffffff" :autoBack="true"
 			leftIconSize="40" />
 		<view style="margin-top: 40rpx;">
-			<u-tabs :list="list" @click="tabsClick" lineWidth="140rpx" lineHeight="4rpx" :activeStyle="activeStyle"
+			<u-tabs :list="tabList" @click="tabsClick" lineWidth="140rpx" lineHeight="4rpx" :activeStyle="activeStyle"
 				:scrollable="false"></u-tabs>
 		</view>
 		<view v-if="index==0">
@@ -11,7 +11,7 @@
 				:show="show" iconSize="240" width="400" height="400" text="记录为空">
 			</u-empty>
 			<my-learn-record :show="!show" :status="status" :loading-text="loadingText" :loadmore-text="loadmoreText"
-				:nomore-text="nomoreText" :listIndex="listIndex" @loadmore="loadmore"></my-learn-record>
+				:nomore-text="nomoreText" :list="list" @loadmore="loadmore"></my-learn-record>
 
 		</view>
 		<view v-if="index==1">
@@ -19,7 +19,7 @@
 				:show="show" iconSize="240" width="400" height="400" text="记录为空">
 			</u-empty>
 			<my-pay-record :show="!show" :status="status" :loading-text="loadingText" :loadmore-text="loadmoreText"
-				:nomore-text="nomoreText" :listIndex="listIndex" @loadmore="loadmore"></my-pay-record>
+				:nomore-text="nomoreText" :list="list" @loadmore="loadmore"></my-pay-record>
 		</view>
 		<view v-if="index==2">
 			<u-empty marginTop="140" mode="order" icon="http://cdn.uviewui.com/uview/empty/order.png" textSize="36"
@@ -38,10 +38,14 @@
 </template>
 
 <script>
+	import {
+		getOrder,
+		getLearnRecord
+	} from '@/config/api.js'
 	export default {
 		data() {
 			return {
-				list: [{
+				tabList: [{
 						name: '学习记录'
 					},
 					{
@@ -65,15 +69,20 @@
 				loadingText: '努力加载中,先喝杯茶',
 				loadmoreText: '上拉或点击加载更多',
 				nomoreText: '我也是有底线的',
-				page: 0,
-				listIndex: 4
+				size: 0,
+				list: [],
+				total: 0,
+				params: {
+					pageNum: 1,
+					pageSize: 3
+				}
 
 			}
 		},
 		methods: {
-			tabsClick(index) {
+			async tabsClick(index) {
 				console.log(index)
-
+				this.params.pageNum = 1
 				this.index = index.index
 				if (this.index == 2 || this.index == 3) {
 					this.show = true
@@ -81,37 +90,159 @@
 					this.show = false
 				}
 				if (this.index == 0) {
-					this.page = 0
-					this.listIndex = 4
-					this.status = 'loadmore'
+					const {
+						data: order
+					} = await getLearnRecord({
+						params: this.params,
+						custom: {
+							auth: true
+						}
+					})
+					console.log("getLearnRecord--------")
+					console.log(order)
+					this.list = order.rows
+					this.total = order.total
+					this.size = order.rows.length
+					if (order.total > 4) {
+						this.status = 'loadmore'
+					} else {
+						this.status = 'nomore'
+					}
 				} else if (this.index == 1) {
-					this.page = 0
-					this.listIndex = 2
-					this.status = 'loadmore'
+
+					const {
+						data: order
+					} = await getOrder({
+						params: this.params,
+						custom: {
+							auth: true
+						}
+					})
+					console.log("getOrder------")
+					console.log(order)
+					this.list = order.rows
+					this.total = order.total
+					this.size = order.rows.length
+					if (order.total > 2) {
+						this.status = 'loadmore'
+					} else {
+						this.status = 'nomore'
+					}
 				}
 				console.log(this.index)
 			},
-			loadmore() {
+			async loadmore() {
 				console.log("loadmore")
-				if (this.page >= 3) return;
+				if (this.size == this.total) return;
 				this.status = 'loading';
-				this.page = ++this.page;
+				if (this.index == 0) {
+					this.params.pageNum++;
+					console.log(this.params)
+					const {
+						data: order
+					} = await getLearnRecord({
+						params: this.params,
+						custom: {
+							auth: true
+						}
+					})
+
+					setTimeout(() => {
+						this.list = [...this.list, ...order.rows]
+						console.log(this.list)
+						this.size += order.rows.length
+						if (this.size == this.total) this.status = 'nomore';
+						else this.status = 'loading';
+					}, 1000)
+				} else if (this.index == 1) {
+					this.params.pageNum++;
+					console.log(this.params)
+					const {
+						data: order
+					} = await getOrder({
+						params: this.params,
+						custom: {
+							auth: true
+						}
+					})
+
+					setTimeout(() => {
+						this.list = [...this.list, ...order.rows]
+						console.log(this.list)
+						this.size += order.rows.length
+						if (this.size == this.total) this.status = 'nomore';
+						else this.status = 'loading';
+					}, 1000)
+				}
+			},
+			async listInit() {
+				const {
+					data: order
+				} = await getLearnRecord({
+					params: this.params,
+					custom: {
+						auth: true
+					}
+				})
+				console.log("getOrder------")
+				console.log(order)
+				this.list = order.rows
+				this.total = order.total
+				this.size = order.rows.length
+				if (order.total > 4) {
+					this.status = 'loadmore'
+				} else {
+					this.status = 'nomore'
+				}
+			}
+		},
+		async onReachBottom() {
+			console.log("onReachBottom")
+			if (this.size == this.total) return;
+			this.status = 'loading';
+
+			if (this.index == 0) {
+				this.params.pageNum++;
+				console.log(this.params)
+				const {
+					data: order
+				} = await getLearnRecord({
+					params: this.params,
+					custom: {
+						auth: true
+					}
+				})
+
 				setTimeout(() => {
-					this.listIndex += 4;
-					if (this.page >= 3) this.status = 'nomore';
+					this.list = [...this.list, ...order.rows]
+					console.log(this.list)
+					this.size += order.rows.length
+					if (this.size == this.total) this.status = 'nomore';
+					else this.status = 'loading';
+				}, 1000)
+			} else if (this.index == 1) {
+				this.params.pageNum++;
+				console.log(this.params)
+				const {
+					data: order
+				} = await getOrder({
+					params: this.params,
+					custom: {
+						auth: true
+					}
+				})
+
+				setTimeout(() => {
+					this.list = [...this.list, ...order.rows]
+					console.log(this.list)
+					this.size += order.rows.length
+					if (this.size == this.total) this.status = 'nomore';
 					else this.status = 'loading';
 				}, 1000)
 			}
 		},
-		onReachBottom() {
-			if (this.page >= 3) return;
-			this.status = 'loading';
-			this.page = ++this.page;
-			setTimeout(() => {
-				this.listIndex += 4;
-				if (this.page >= 3) this.status = 'nomore';
-				else this.status = 'loading';
-			}, 1000)
+		onLoad() {
+			this.listInit()
 		}
 
 	}
