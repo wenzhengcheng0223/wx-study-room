aff106bbf04242989d818e96e82e336259f90ac5
wenzhengcheng <wenzhengcheng0223@163.com>
2022-05-16 21:51:29 +0800

    添加 卡券页面组件

---
+++ components/my-seat-area/my-seat-area.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<u--text v-if="areaNum==0" :bold="true" size="28" color="#35a5ed" text="经济区" align="center" />
+		<u--text v-else-if="areaNum==1" :bold="true" size="28" color="#35a5ed" text="舒适区" align="center" />
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			areaNum: {
+				type: Number
+			}
+		},
+		name: "my-seat-area",
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
